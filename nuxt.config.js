cc8a7c3a2739dcef93124035e17ed564b1078ccb
Usuário <usuario@10-46-49-48.0200.prio.br.iosda.org>
2025-01-29 18:03:17 -0300

    inicial 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,10 @@ export default {
     '@nuxt/content',
   ],
 
+  app: {
+    baseURL: "/Portfolio/",
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
